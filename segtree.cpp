a31215f457adb7fb53616d9bc59eca1a3194138a
Revathi-Bonala <144534433+Revathi-Bonala@users.noreply.github.com>
2024-03-21 02:42:14 -0700

    segtree.cpp

--- segtree.cpp
+++ segtree.cpp
@@ -1,44 +1,53 @@
 #include<bits/stdc++.h>
 using namespace std;
-void build(int ind,int low,int high,int arr[],int seg[]){
+ 
+class SGT_tree{
+   
+    public:
+     vector<int>seg;
+    SGT_tree(int n){
+      seg.resize(4*n+1);
+    }
+void build(int ind,int low,int high,int arr[]){
     if(low==high) {
         seg[ind]=arr[low];
         return;
     }
     int mid=(low+high)/2;
-    build(2*ind+1,low,mid,arr,seg);
-    build(2*ind+2,mid+1,high,arr,seg);
+    build(2*ind+1,low,mid,arr);
+    build(2*ind+2,mid+1,high,arr);
    seg[ind]=min(seg[2*ind+1],seg[2*ind+2]);
 }
-int query(int ind,int low,int high,int l,int r,int seg[]){
+int query(int ind,int low,int high,int l,int r){
     //no overlap
     if(l>high || r<low) return INT_MAX;
     //complete overlap
     if(low>=l && high<=r) return seg[ind];
     int mid=(low+high)>>1;
-    int left=query(2*ind+1,low,mid,l,r,seg);
-    int right=query(2*ind+2,mid+1,high,l,r,seg);
+    int left=query(2*ind+1,low,mid,l,r);
+    int right=query(2*ind+2,mid+1,high,l,r);
     return min(left,right);
 }
-void update(int ind,int low,int high,int i,int val,int seg[]){
+void update(int ind,int low,int high,int i,int val){
     if(low==high){
         seg[ind]=val;
         return;
     }
     int mid=(low+high)/2;
-    if(i<=mid) update(2*ind+1,low,mid,i,val,seg);
-    else update(2*ind+2,mid+1,high,i,val,seg);
+    if(i<=mid) update(2*ind+1,low,mid,i,val);
+    else update(2*ind+2,mid+1,high,i,val);
     seg[ind]= min(seg[2*ind+1],seg[2*ind+2]);
 
 }
+};
 int main(){
     int n;
     cin>>n;
     int arr[n];
     for(int i=0;i<n;i++) cin>>arr[i];
-    int seg[4*n];
-    build(0,0,n-1,arr,seg);
-    cout<<seg[0]<<endl;
+   SGT_tree sg(n);
+    sg.build(0,0,n-1,arr);
+    cout<<sg.seg[0]<<endl;
     int q;
     cin>>q;
     // while(q--){
@@ -54,13 +63,13 @@ int main(){
         {
             int l,r;
             cin>>l>>r;
-            cout<<query(0,0,n-1,l,r,seg)<<endl;
+            cout<<sg.query(0,0,n-1,l,r)<<endl;
 
         }
         else{
             int i,val;
             cin>>i>>val;
-            update(0,0,n-1,i,val,seg);
+            sg.update(0,0,n-1,i,val);
           arr[i]=val;
           for(int i=0;i<n;i++ ) cout<<arr[i]<<" ";
           cout<<endl;
